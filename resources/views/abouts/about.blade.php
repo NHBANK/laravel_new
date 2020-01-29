a7b3a6ae34d6fd95340caaa5858590cb40ac3405
Dandooshake <imbrian618@gmail.com>
2020-01-29 14:34:19 +0900

    Second Update

--- resources/views/abouts/about.blade.php
+++ resources/views/abouts/about.blade.php
@@ -6,12 +6,27 @@
 
 @section('content')
     <div class="px-64 mt-4">
-        <h1 class="font-bold text-3xl flex-1">Intro</h1>
+        <h1 class="font-bold text-3xl flex-1">Intro</h1><br>
             
             This is a Laravel 5.8 tutorial page.<br>
             <br>
             If you wanna be a sponsor, pay $10000 every month.<br>
             Account :  <u>1002-353-331235</u> <br>  
         <br>
+        <br>
+        <br>
+        
+        <h1 class="font-bold text-3xl flex-1">Located at</h1><br>
+        <a href = "http://kko.to/VWKU8_e0T" target="_blank"><u>Sungsu-dong 2-ga, Sungdong-Gu, Seoul-Si</u></a>
+        <br>
+        <br>
+        <a href="https://map.kakao.com/?urlX=511935&urlY=1124026&urlLevel=3&map_type=TYPE_MAP&map_hybrid=false" target="_blank">
+        <img width="504" height="310" src="https://map2.daum.net/map/mapservice?FORMAT=PNG&SCALE=2.5&MX=511935&MY=1124026&S=0&IW=504&IH=310&LANG=0&COORDSTM=WCONGNAMUL&logo=kakao_logo" 
+        style="border:1px solid #ccc"></a><div class="hide" style="overflow:hidden;padding:7px 11px;border:1px solid #dfdfdf;border-color:rgba(0,0,0,.1);border-radius:0 0 2px 2px;background-color:#f9f9f9;width:482px;">
+        <strong style="float: left;"><img src="//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/logo_kakaomap.png" width="72" height="16" alt="카카오맵"></strong><div style="float: right;position:relative">
+        <a style="font-size:12px;text-decoration:none;float:left;height:15px;padding-top:1px;line-height:15px;color:#000" target="_blank" href="https://map.kakao.com/?urlX=511935&urlY=1124026&urlLevel=3&map_type=TYPE_MAP&map_hybrid=false">지도 크게 보기</a></div></div>
+        
+        <br>
     </div>
 @endsection
+
--- resources/views/contacts/contact.blade.php
+++ resources/views/contacts/contact.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="px-64 mt-4">
         <h1 class="font-bold text-3xl flex-1">Location</h1><br> 
-        <u>Baekbomro, Mapo-Gu, Seoul-Si</u>
+        <a href = "http://kko.to/p3ww8VA0H" target="_blank"><u>Baekbomro, Mapo-Gu, Seoul-Si</u></a>
         <br>
         <br>
         
@@ -16,9 +16,10 @@
         <div class="hide" style="overflow:hidden;padding:7px 11px;border:1px solid #dfdfdf;border-color:rgba(0,0,0,.1);border-radius:0 0 2px 2px;background-color:#f9f9f9;width:482px;"><strong style="float: left;">
         <img src="//t1.daumcdn.net/localimg/localimages/07/2018/pc/common/logo_kakaomap.png" width="72" height="16" alt="카카오맵"></strong><div style="float: right;position:relative">
         <a style="font-size:12px;text-decoration:none;float:left;height:15px;padding-top:1px;line-height:15px;color:#000" target="_blank" href="https://map.kakao.com/?urlX=486582&urlY=1125872&urlLevel=4&map_type=TYPE_MAP&map_hybrid=false">지도 크게 보기</a></div></div>
-
         <br>
         <br>
+        <br>
+        
         <h1 class="font-bold text-3xl flex-1">Contact</h1><br>
         <u>02-705-8114</u>
     </div>
